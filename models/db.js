1da29f360d02601bc1162d8eded85b1cab8127dc
Tushar032 <tushar.bagga.456@gmail.com>
2024-01-18 19:22:32 +0530

    Completed the attendance module

---
+++ models/db.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+module.exports.init = async function () {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Server is connected to database");
+};
